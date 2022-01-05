0a615314199cf191c14efc8a9d8c99798dd51e49
RANA ABOBAKAR <ranaabobakarit@gmail.com>
2022-01-05 12:44:41 +0500

    add logging for query not show in terminal

--- model/database.js
+++ model/database.js
@@ -1,4 +1,7 @@
 const { Sequelize } = require("sequelize");
-
-database=new Sequelize('test','root','',{host:'localhost',dialect:'mariadb'})
-module.exports=database;
+database = new Sequelize("test", "root", "", {
+  host: "localhost",
+  dialect: "mariadb",
+  logging: false,
+}); //logging false :use for query not show in terminal
+module.exports = database;
